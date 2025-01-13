70418400285a95c786ece951ce7ba2846461393d
houssamlambara <lambarahoussam@gmail.com>
2025-01-13 19:03:41 +0100

    signup ajouter au base de donner

--- login/signup.php
+++ login/signup.php
@@ -1,3 +1,26 @@
+<?php
+session_start();
+include_once "../classes/database.php";
+include_once "../classes/class_user.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $role = $_POST["role"];
+
+       if ($email && $password) {
+        User::signup($nom, $prenom, $email, $password, $role);
+        
+        echo "User created successfully.";
+    } else {
+        echo "Email and password are required.";
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -59,10 +82,10 @@
                 <div class="w-full p-8"> 
                     <h2 class="text-2xl font-semibold text-center text-gray-800 mb-4">Créer un compte Youdemy</h2> 
 
-                    <form action="/signup" method="POST" class="space-y-4"> 
+                    <form action="" method="POST" class="space-y-4"> 
                         <div>
                             <label for="name" class="block text-sm font-medium text-gray-700">Nom</label>
-                            <input type="text" id="name" name="name" required placeholder="Votre nom"
+                            <input type="text" id="name" name="nom" required placeholder="Votre nom"
                                 class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none transition duration-300 ease-in-out" /> <!-- Réduit padding -->
                         </div>
                         <div>
@@ -81,11 +104,11 @@
                                 class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none transition duration-300 ease-in-out" />
                         </div>
                         <div>
-                            <label for="role" class="block text-sm font-medium text-gray-700">Rôle</label>
-                            <select id="role" name="role" required
+                            <label for="" class="block text-sm font-medium text-gray-700">Rôle</label>
+                            <select id="role " name="role" required
                                 class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none transition duration-300 ease-in-out">
-                                <option value="student">Étudiant</option>
-                                <option value="teacher">Enseignant</option>
+                                <option value="1">Étudiant</option>
+                                <option value="2">Enseignant</option>
                             </select>
                         </div>
                         <div>
