dec66570425cd6da736fa6a7e795af4ca3298220
houssamlambara <lambarahoussam@gmail.com>
2025-01-15 21:33:13 +0100

    categorie update pour pouvoir ajouter des categories

--- classes/class_categorie.php
+++ classes/class_categorie.php
@@ -39,7 +39,7 @@ class Categorie
             $stmt->bindParam(':nom', $this->nom, PDO::PARAM_STR);
 
             if ($stmt->execute()) {
-                $this->id = $db->lastInsertId();  
+                $this->id = $db->lastInsertId();
                 return true;
             } else {
                 echo "Erreur lors de l'exécution de la requête : " . $stmt->errorInfo()[2];
@@ -50,6 +50,26 @@ class Categorie
             throw new Exception("Erreur lors de l'ajout de la catégorie : " . $e->getMessage());
         }
     }
+
+    public static function getAllCategorie()
+    {
+        $categories = [];
+        $db = Database::getInstance()->getConnection();
+        try {
+            $req = "SELECT id, nom FROM categories";
+            $stmt = $db->prepare($req);
+            $stmt->execute();
+            $arry = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($arry as $row) {
+                $categories[] = new Categorie($row['nom'], $row['id']);
+            }
+            return $categories;
+        } catch (PDOException $e) {
+            echo "Erreur lors de la récupération des catégories : " . $e->getMessage();
+            return [];
+        }
+    }
+
 
     public static function getAll()
     {
