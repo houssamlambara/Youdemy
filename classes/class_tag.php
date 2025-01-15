0af518a3a06a461caefd4de94afaced1e9902f6d
houssamlambara <lambarahoussam@gmail.com>
2025-01-15 12:14:09 +0100

    ajouter class_tag

---
+++ classes/class_tag.php
@@ -0,0 +1,76 @@
+<?php
+
+include_once('database.php');
+
+class tag
+{
+    private $id;
+    private $nom;
+
+    public function __construct($nom, $id = null)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+    }
+
+    public function __toString()
+    {
+        return $this->id . " " . $this->nom;
+    }
+
+    // Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function save()
+    {
+        try {
+            $db = Database::getInstance()->getConnection();
+
+            $stmt = $db->prepare("INSERT INTO tags (nom) VALUES (:nom)");
+
+            $stmt->bindParam(':nom', $this->nom, PDO::PARAM_STR);
+
+            if ($stmt->execute()) {
+                $this->id = $db->lastInsertId();  
+                return true;
+            } else {
+                echo "Erreur lors de l'exécution de la requête : " . $stmt->errorInfo()[2];
+            }
+
+            return false;
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de l'ajout de la catégorie : " . $e->getMessage());
+        }
+    }
+
+    public static function getAll()
+    {
+        try {
+            $db = Database::getInstance()->getConnection();
+            $stmt = $db->query("SELECT * FROM tags");
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la récupération des catégories : " . $e->getMessage());
+        }
+    }
+
+    public static function delete($id)
+    {
+        try {
+            $db = Database::getInstance()->getConnection();
+            $stmt = $db->prepare("DELETE FROM tags WHERE id = :id");
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la suppression de la catégorie : " . $e->getMessage());
+        }
+    }
+}
